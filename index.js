e13d1acbebcca86d9301f54f4bcf6f3e6a24a610
Melissa Xie <mxie@github.com>
2019-11-06 15:52:38 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const projectId = core.getInput('project-id');
+  console.log(`Project ID: ${project}`);
+
+  const srcColumnId = core.getInput('src-column-id');
+  console.log(`Source: ${srcColumnId}`);
+
+  const destColumnId = core.getInput('dest-column-id');
+  console.log(`Destination: ${destColumnId}`);
+} catch (error) {
+  core.setFailed(error.message)
+}
