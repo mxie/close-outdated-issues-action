48d6582b5dc2de6064eaecebb788ec61594cc3b0
Melissa Xie <mxie@github.com>
2020-04-25 14:54:03 -0400

    Just close issues past the specified date instead of using projects
    Baby steps!

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
-  const projectId = core.getInput('project-id');
-  console.log(`Project ID: ${projectId}`);
+async function run() {
+  const token = core.getInput("token");
+  const octokit = new github.GitHub(token);
+  const ghContext = github.context;
 
-  const srcColumnId = core.getInput('src-column-id');
-  console.log(`Source: ${srcColumnId}`);
+  const { data: openIssues } = await octokit.issues.listForRepo({
+    ...ghContext.repo,
+    state: "open",
+  });
 
-  const destColumnId = core.getInput('dest-column-id');
-  console.log(`Destination: ${destColumnId}`);
-} catch (error) {
-  core.setFailed(error.message)
+  const datePattern = /(\d\d).(\d\d).\d{2,4}/;
+  const today = new Date();
+
+  console.log(`Found ${openIssues.length} open issues.`);
+
+  let issuesClosed = 0;
+
+  for (const issue of openIssues) {
+    const issueNumber = issue.number;
+    const issueTitle = issue.title;
+
+    const results = datePattern.exec(issueTitle);
+
+    if (results !== null) {
+      const [month, day] = results.slice(1);
+
+      const issueDate = new Date(2020, month - 1, day);
+
+      if (issueDate < today) {
+        octokit.issues.update({
+          ...ghContext.repo,
+          issue_number: issueNumber,
+          status: "closed",
+        });
+
+        issuesClosed++;
+      }
+    }
+  }
+
+  console.log(`Closed ${issuesClosed} issues.`);
 }
+
+run();
